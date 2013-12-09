7db758706b1a1ab879a33778a9559abcc7a35cf5
JonathanTech <jon.ross@sproutboard.com>
2013-12-09 01:45:48 -0600

    finished features

--- app/js/app.js
+++ app/js/app.js
@@ -5,17 +5,91 @@
 var app = angular.module('app', [ ]);
 
 
-app.controller('mainController', ['$scope','ParsePackageDependenciesService',
-    function($scope, ParsePackageDependenciesService) {
-        $scope.change = function(){
+app.controller('mainController', ['$scope', 'ParsePackageDependenciesService',
+    function ($scope, ParsePackageDependenciesService) {
+        $scope.change = function () {
             $scope.output = ParsePackageDependenciesService.parse($scope.input);
         }
     }]);
 
-app.factory('ParsePackageDependenciesService', function(){
-    var parse = function(a){return a;}
+app.factory('ParsePackageDependenciesService', function () {
+    var createPackageObjects = function (inputArray) {
+            var i, tempArray, packages = [];
+            try {
+                for (i = 0; i < inputArray.length; i++) {
+                    tempArray = inputArray[i].split(':');
+                    packages[i] = {name: tempArray[0], requires: tempArray[1].trim()};
+                }
+            }
+            catch (error) {
+                throw  "PARSE ERROR: line " + (i + 1);
+            }
+            return packages;
+        },
+        getDependencies = function(currentPackage, packages, dependencyHistory){
+            var dhI, pI, nextPackage, returnedDependencies;
+            if(currentPackage.requires === '')
+            {
+                returnedDependencies = [];
+                returnedDependencies.push(currentPackage.name);
+                return returnedDependencies;
+            }
+            else {
+                for(dhI = 0; dhI < dependencyHistory.length; dhI++){
+                    if(currentPackage.requires === dependencyHistory[dhI]){
+                        dependencyHistory.push(currentPackage.name);
+                        dependencyHistory.push("*"+currentPackage.requires+"*");
+
+                        throw "ERROR:Cyclical reference - " + dependencyHistory.join(', ').trim();
+                    }
+                }
+                for(pI = 0; pI < packages.length; pI++){
+                    if(packages[pI].name === currentPackage.requires){
+                        nextPackage =  packages[pI];
+                        dependencyHistory.push(currentPackage.name);
+                        returnedDependencies = getDependencies(nextPackage, packages, dependencyHistory);
+                        returnedDependencies.push(currentPackage.name);
+                        return returnedDependencies;
+                    }
+
+                }
+
+                throw "ERROR: missing required package '" + currentPackage.requires + "'";
+
+            }
+        },
+        parseDependencies = function( packages){
+            var i, j, dependencyHistory,
+                currentPackage, dependenciesInOrder = [],
+                distinctDependencies = [];
+            for (i = 0; i < packages.length; i++) {
+                dependencyHistory = [];
+                currentPackage = packages[i];
+                dependenciesInOrder = dependenciesInOrder.concat( getDependencies(currentPackage, packages, dependencyHistory));
+            }
+            for(i = 0; i < dependenciesInOrder.length; i++){
+                if(distinctDependencies.indexOf(dependenciesInOrder[i]) < 0)
+                    distinctDependencies.push(dependenciesInOrder[i]);
+            }
+            return distinctDependencies;
+        },
+        parse = function (input) {
+            var inputArray = input.split('\n'),
+                packages,
+                dependencies;
+            try {
+                packages = createPackageObjects(inputArray)
+                dependencies =  parseDependencies(packages);
+            }
+            catch (error) {
+                return error;
+            }
+
+            return dependencies.join(', ');
+        }
+
 
     return{
-        parse:parse
+        parse: parse
     }
 })
--- test/unit/appSpec.js
+++ test/unit/appSpec.js
@@ -1,33 +1,91 @@
 'use strict';
 
 /* jasmine specs for controllers go here */
-describe('controllers', function() {
+describe('controllers', function () {
 
-  beforeEach(module('app'));
+    beforeEach(module('app'));
 
-  describe('mainController', function(){
-    var scope, ctrl, _mockParsingService, _expectedParsedReturnValue;
+    describe('mainController', function () {
+        var scope, ctrl, _mockParsingService, _expectedParsedReturnValue;
 
-    beforeEach(inject(function( $rootScope, $controller, ParsePackageDependenciesService ) {
+        beforeEach(inject(function ($rootScope, $controller) {
 
-      scope = $rootScope.$new();
-      _mockParsingService = {parse:  function(){   }};
+            scope = $rootScope.$new();
+            _mockParsingService = {parse: function () {
+            }};
 
 
-      ctrl = $controller('mainController', {$scope: scope, "ParsePackageDependenciesService":_mockParsingService});
-    }));
-    it('should receive input in $scope.input when $scope.change is triggered and output the results to $scope.output', function(){
-        _expectedParsedReturnValue = "bob";
-        var expectedInput =  "abcd:asdf123";
-        spyOn(_mockParsingService, 'parse').andCallFake(function(){
-            return _expectedParsedReturnValue;
-        });
-        scope.input = expectedInput;
-        scope.change();
-        expect( scope.output).toBe(_expectedParsedReturnValue);
-        expect(_mockParsingService.parse).toHaveBeenCalledWith(expectedInput);
+            ctrl = $controller('mainController', {$scope: scope, "ParsePackageDependenciesService": _mockParsingService});
+        }));
+        it('should receive input in $scope.input when $scope.change is triggered and output the results to $scope.output', function () {
+            _expectedParsedReturnValue = "bob";
+            var expectedInput = "abcd:asdf123";
+            spyOn(_mockParsingService, 'parse').andCallFake(function () {
+                return _expectedParsedReturnValue;
+            });
+            scope.input = expectedInput;
+            scope.change();
+            expect(scope.output).toBe(_expectedParsedReturnValue);
+            expect(_mockParsingService.parse).toHaveBeenCalledWith(expectedInput);
+        })
+    });
+
+    describe('ParsePackageDependenciesService', function () {
+        var  parsePackageDependenciesSvc;
+        beforeEach(inject(function($injector){
+
+            parsePackageDependenciesSvc = $injector.get('ParsePackageDependenciesService');
+        }))
+        describe('invalid Input tests', function(){
+            it('should return an error if references are cyclical', function(){
+                var input = "KittenService:\n"+
+                "Leetmeme: Cyberportal\n" +
+                "Cyberportal: Ice\n" +
+                "CamelCaser: KittenService\n" +
+                "Fraudstream:\n" +
+                "Ice: Leetmeme",
+                expectedOutput = "ERROR:Cyclical reference - Leetmeme, Cyberportal, Ice, *Leetmeme*",
+                output = parsePackageDependenciesSvc.parse(input);
+                expect(output).toBe(expectedOutput);
+            })
+            it('should return an error if input is malformed', function(){
+                var input = "KittenService:\n"+
+                    "Leetmeme",
+
+                expectedOutput ="PARSE ERROR: line 2",
+                output = parsePackageDependenciesSvc.parse(input);
+                expect(output).toBe(expectedOutput);
+            })
+
+            it('should return an error if required package is missing', function(){
+                var input = "KittenService:Bad\n"+
+                        "Leetmeme:",
+
+                    expectedOutput ="ERROR: missing required package 'Bad'",
+                    output = parsePackageDependenciesSvc.parse(input);
+                expect(output).toBe(expectedOutput);
+            })
+        })
+        describe('Valid input tests', function(){
+            it('should accept valid input #1', function(){
+                var input = 'KittenService:\n'+
+                    'Leetmeme: Cyberportal\n'+
+                'Cyberportal: Ice\n'+
+                'CamelCaser: KittenService\n'+
+                'Fraudstream: Leetmeme\n'+
+                'Ice:',
+                expectedOutput = 'KittenService, Ice, Cyberportal, Leetmeme, CamelCaser, Fraudstream',
+                output = parsePackageDependenciesSvc.parse(input);
+                expect(output).toBe(expectedOutput);
+            })
+            it('should accept valid input #2', function(){
+                var input = 'KittenService: CamelCaser\n'+
+                'CamelCaser:',
+                expectedOutput = 'CamelCaser, KittenService',
+                output = parsePackageDependenciesSvc.parse(input);
+                expect(output).toBe(expectedOutput);
+            })
+        })
     })
-  });
-
 
 });
